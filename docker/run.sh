2e5c26180f19bdacc4e31131f3d7b23f52a1d5c4
Zeppelins-Forever <128062431+Zeppelins-Forever@users.noreply.github.com>
2026-01-19 23:23:08 -0600

    Refactor run.sh with additional comments and output logic
    Updated run.sh to include commented-out commands and modify output handling.

--- docker/run.sh
+++ docker/run.sh
@@ -1,11 +1,30 @@
 #!/bin/sh
-
-# Puts our environment into our Python virtual environment (venv):
 source myenv/bin/activate
 
-# Potential alternate command. Currently, it just outputs videos as "[date] video name (ID).mp4.mp4". 
-# Not ideal, but keeping it around for reference.
-# python3 runner.py --log-level DEBUG --output "/out/[%(upload_date)s] %(title)s (%(id)s).%(ext)s"  --resolution best $1
+#########################
+#python3 runner.py --log-level DEBUG --output "/out/[%(upload_date)s] %(title)s (%(id)s).%(ext)s"  --resolution best $1
+#python3 runner.py --log-level DEBUG --embed-thumbnail --wait-for-video 60 --output "/out/[%(upload_date)s] %(title)s (%(id)s)"  --resolution best $1
+#python3 runner.py --log-level DEBUG --embed-thumbnail --wait-for-video 60 --live-chat --output "/out/[%(upload_date)s] %(title)s (%(id)s"  --resolution best $1
 
-# This runs CanOfSocks's livestream_dl program.
-python3 runner.py --log-level DEBUG --output "/out/[%(upload_date)s] %(title)s (%(id)s)" --resolution best $1
+#  If I want the container to automatically use cookies if /cookies/cookies.txt exists:
+#COOKIE_FILE="/cookies/cookies.txt"
+#if [ -f "$COOKIE_FILE" ]; then
+#    # If the cookies file exists, inject the --cookies flag before the rest of the arguments
+#    echo "Found /cookies/cookies.txt, applying to runner..."
+#    python3 runner.py --cookies "$COOKIE_FILE" "$@"
+#else
+#    # Otherwise, run normally
+#    python3 runner.py "$@"
+#fi
+#########################
+
+# Automatically passes all arguments to runner.py (not including arguments for Docker itself, such as -v "/mounted/filesystem/")
+echo "$@"
+python3 runner.py --output "/temp/[%(upload_date)s] %(title)s (%(id)s)" "$@"
+# At some point, add custom user argument to allow customizing file name (though filter certain characters, preventing user from changing file location accidentally)
+
+# Necessary currently since certain functions in the container need Root to run, but this conflicts with
+# permissions outside the contamv iner. Therefore, need intermediary storage location inside the container.
+#chmod +x /temp/*
+chown -R ${MY_UID}:${MY_GID} /temp/*
+mv /temp/* /out/
