8c2fb30bb6eda1f4db707c17c66b2692c3a87ced
Zeppelins-Forever <128062431+Zeppelins-Forever@users.noreply.github.com>
2026-01-19 23:04:54 -0600

    Update log level prompts in quick-builds.sh

--- dev-help/quick-builds.sh
+++ dev-help/quick-builds.sh
@@ -1,7 +1,7 @@
 echo "Run this in the same directory as the Go livestream_dl_containerized project for building archive-helper."
 mkdir Releases
 
-read -n 1 -s -r -p "Make sure the .go file is set to output INFO logs.\nPress any key to continue..."
+read -n 1 -s -r -p "Make sure the .go file is set to --log-level INFO logs.\nPress any key to continue..."
 echo
 # INFO Builds - change the
 GOOS=linux GOARCH=amd64 go build -o Releases/archive-helper-INFO-Linux-x64
@@ -10,7 +10,7 @@ GOOS=windows GOARCH=amd64 go build -o Releases/archive-helper-INFO-Windows-x64
 GOOS=windows GOARCH=arm64 go build -o Releases/archive-helper-INFO-Windows-ARM64.exe
 GOOS=darwin GOARCH=arm64 go build -o Releases/archive-helper-INFO-MacOS-AppleSilicon
 
-read -n 1 -s -r -p "Make sure the .go file is set to output DEBUG logs.\nPress any key to continue..."
+read -n 1 -s -r -p "Make sure the .go file is set to --log-level DEBUG logs.\nPress any key to continue..."
 echo
 # DEBUG Builds
 GOOS=linux GOARCH=amd64 go build -o Releases/archive-helper-DEBUG-Linux-x64
